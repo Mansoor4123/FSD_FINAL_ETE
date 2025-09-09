31d1ec8d9d35a85801ebdee1be4d32f62e3511ba
M M Mohamed Mansoor <mmohamed.mansoor2004@gmail.com>
2025-09-09 11:44:03 +0530

    done

--- pet-dog/src/App.jsx
+++ pet-dog/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       </main>
 
       {/* Footer */}
-      <footer className="bg-purple-700 text-white py-4 mt-8 text-center shadow-inner">
+      <footer className="bg-purp-700 text-white py-4 mt-8 text-center shadow-inner">
         <span className="text-sm">&copy; {new Date().getFullYear()} Dog Breed Explorer. Made with React, Vite & Tailwind CSS.</span>
       </footer>
     </div>
